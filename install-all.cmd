ba0a2558909c7a527b0fff616604aad8ed081a1b
joelam789 <joelam789@yahoo.com.hk>
2019-05-03 10:44:53 +0800

    restrict jspm version to 0.16.x

--- install-all.cmd
+++ install-all.cmd
@@ -1,9 +1,8 @@
 
 npm init
-
-npm install --save-dev typescript jspm
-REM npm install --save-dev electron json-stringify-deterministic
+npm install --save-dev typescript jspm@0.16x
 npm install --save-dev del gulp gulp-typescript gulp-sourcemaps gulp-webserver
+REM npm install --save-dev electron json-stringify-deterministic
 
 npm install --save core-js i18next-xhr-backend
 npm install --save aurelia-framework aurelia-bootstrapper aurelia-router aurelia-event-aggregator aurelia-pal-browser aurelia-dialog aurelia-i18n
